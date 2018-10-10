e85dcb69bcc418b7538180085372145f3e267a49
qiahao <404546074@qq.com>
2018-10-10 14:05:05 +0800

    update

--- src/route/index.js
+++ src/route/index.js
@@ -1,14 +1,20 @@
 import Home from '@/views/Home.vue'
 
-export default [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import('@/views/About.vue')
-  }
+export default [{
+  path: '/',
+  name: 'home',
+  component: Home
+},
+{
+  path: '/about',
+  name: 'about',
+  component: () =>
+      import('@/views/About.vue')
+},
+{
+  path: '/add',
+  name: 'add',
+  component: () =>
+      import('@/views/Add.vue')
+}
 ]
---
+++ src/views/Add.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <ul>
+      <li v-for="(item, index) in list" :key="index">{{item.name}} {{item.age}} {{item.birthday}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+import { userApi } from '@api'
+export default {
+  data() {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    getList() {
+      userApi.getList().then(data => {
+        this.list = data.data
+      })
+    }
+  },
+  created() {
+    this.getList()
+  }
+}
+</script>
